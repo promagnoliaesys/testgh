ceb63c448def8dc0fd673affb46ab9f479fad67d
“promagnoliaesys” <paolo.romagnoli@aesys.tech>
2024-05-02 14:33:17 +0200

    lezioni 8 e 9

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+// import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation"
+
+const PropertyPage = () => {
+  //   const router = useRouter()
+  //   const searchParams = useSearchParams()
+  //   const { id } = useParams()
+  //   const name = searchParams.get("name")
+  //   const pathname = usePathname();
+  return <div>Property Page</div>
+}
+
+export default PropertyPage
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,9 +1,12 @@
-const PropertyPage = () => {
+import Link from 'next/link';
+
+const PropertiesPage = () => {
     return (
         <div>
-            <h1>Properties</h1>
+            <h1 className="text-3xl">Properties</h1>
+            <Link href={'/'}>Go Home</Link>
         </div>
     );
 };
 
-export default PropertyPage;
+export default PropertiesPage;
