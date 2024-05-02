0e28ba3ed5ea3e0458841da5cc736c828fd28bd6
“promagnoliaesys” <paolo.romagnoli@aesys.tech>
2024-05-02 12:09:21 +0200

    Lezione 8

---
+++ app/properties/add/page.tsx
@@ -0,0 +1,9 @@
+const PropertyAddPage = () => {
+    return (
+        <div>
+            <h1>PropertyAddPage</h1>
+        </div>
+    );
+};
+
+export default PropertyAddPage;
---
+++ app/properties/page.tsx
@@ -0,0 +1,9 @@
+const PropertyPage = () => {
+    return (
+        <div>
+            <h1>Properties</h1>
+        </div>
+    );
+};
+
+export default PropertyPage;
