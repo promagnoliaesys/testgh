7515aedd03f9317706abc760e667cdd0f932c1a3
“promagnoliaesys” <paolo.romagnoli@aesys.tech>
2024-05-06 10:56:20 +0200

    Add images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "@/assets/styles/globals.css"
+import Navbar from "@/components/Navbar"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "PropertyPulse | Find the perfect Rental",
   description: "Find your dream rental property",
   keywords: "rental, finde renteals, find properties",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
-  );
+  )
 }
